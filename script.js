23521e050a3e7ca23e1d26ea40f8bb1f30225d05
Yesenia <eliyessie@gmail.com>
2018-04-10 19:45:47 -0700

    Change button color on input

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.getElementById("textinputid").addEventListener("input", function(event) {
+
+    const buttonElement = document.getElementById("buttonid");
+    const userInput = event.target.value;
+
+    const isCorrect = userInput.toLowerCase() === "electric mucus";
+
+    buttonElement.style.backgroundColor = userInput.toLowerCase() === "electric mucus" ? "green": userInput;
+    
+    buttonElement.disabled = !isCorrect;
+
+})
+
+
 
